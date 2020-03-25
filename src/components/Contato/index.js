a3c661e454409f9da2931d186b552a5aede38b14
matheusnovais95 <mths_campos@hotmail.com>
2020-03-24 21:48:57 -0300

    Second commit

--- src/components/Contato/index.js
+++ src/components/Contato/index.js
@@ -14,7 +14,7 @@ export default class Contato extends Component {
             <li><MdMail className="icon"></MdMail> mthscampos@gmail.com</li>
             <li><FaGithub className="icon"></FaGithub> <a target="_blank" href="https://github.com/matheusnovais95">matheusnovais95</a></li>
             <li><FaLinkedin className="icon"></FaLinkedin> <a target="_blank" href="https://www.linkedin.com/in/matheus-campos-135430117/">Matheus Novais de Campos</a></li>
-            <li><FaWhatsapp className="icon"></FaWhatsapp> +55(15)981484135</li>
+            <li><FaWhatsapp className="icon"></FaWhatsapp> +55(15)98148-4135</li>
             <Link className="btn-menu" to="container" smooth={true} duration={900}>
               <FaAngleDoubleUp></FaAngleDoubleUp>
             </Link>
